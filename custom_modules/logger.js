9989280f8520f8f181664d5a9cacad0cb4a64bc6
DF1229 <daanfaber1@gmail.com>
2020-09-01 14:19:36 +0200

    Fixed some bugs
    Updated log file location to be a folder called logs, one directory higher than the logger.js file

--- custom_modules/logger.js
+++ custom_modules/logger.js
@@ -1,17 +1,15 @@
-const Discord = require('discord.js');
 const fs = require('fs');
+module.exports = log;
 
-module.exports = {
-    add(authorTag, description) {
-        const date = new Date();
-        const day = date.day;
-        const month = date.month + 1; // defaults to 0 for january, so +1 for easy reading
-        const year = date.year;
 
-        var nextEntry = `${date.getTime()} - ${authorTag} ${description} \n`;
+var log = function(authorTag, description) {
+    const date = new Date();
+    const day = date.day;
+    const month = date.month + 1; // defaults to 0 for january, so +1 for easy reading
+    const year = date.year;
 
-        fs.appendFile(`${year}-${month}-${day} - log.txt`, nextEntry, 'utf8', (err) => {
-            if (err) return console.error(err);
-        });
+    var nextEntry = `${date.getTime()} - ${authorTag} ${description} \n`;
+
+    fs.appendFile(`../logs/${year}-${month}-${day} - log.txt`, nextEntry, 'utf8', (err) => {
     }
 }
