7ffe5b83361fbade239c261d25da07563e5ace2d
DF1229 <daanfaber1@gmail.com>
2020-09-01 16:28:08 +0200

    Updated ping.js
    Now actually pings, and uses the new logging system

--- commands/ping.js
+++ commands/ping.js
@@ -1,10 +1,24 @@
+const Logger = require('../custom_modules/logger.js');
+const Discord = require('discord.js');
+
 module.exports = {
 	name: 'ping',
     description: 'Pong!',
     args: false,
     guildOnly: false,
     adminOnly: false,
-	execute(message, args) {
-		message.channel.send(':ping_pong:');
+	async execute(msg, args) {
+		let embed = new Discord.MessageEmbed()
+            .setColor('DARK_GOLD')
+            .setTimestamp()
+            .setTitle('Ping :ping_pong: Pong')
+            .setFooter(msg.client.user.tag, msg.client.user.avatarURL);
+
+        const m = await msg.channel.send('Pinging...');
+
+        embed.addField('Ping to bot', `${m.createdTimestamp - msg.createdTimestamp}ms`, true);
+        embed.addField('API Heartbeat', `${Math.round(msg.client.ping)}ms`, true);
+        m.edit(embed);
+        Logger.log(msg.author.tag, `pinged the bot.`);
 	},
 };
