e149b869e5df0afb44625785c7d1a640f480ce3a
DF1229 <daanfaber1@gmail.com>
2020-09-01 16:27:33 +0200

    Updated meme.js
    Now uses new logging system and Discord v12 MessageEmbeds

--- commands/meme.js
+++ commands/meme.js
@@ -1,3 +1,4 @@
+const Logger = require('../custom_modules/logger.js');
 const Discord = require('discord.js');
 const fs = require('fs');
 
@@ -14,25 +15,52 @@ module.exports = {
         fs.readFile('./commands/memepool.json', (err, rawdata) => {
             if (err) {
                 msg.channel.send(`:x: An error occured trying to read the file, ${msg.author}.`);
-                throw new Error(err);
+                console.error(err);
+                return Logger.log(msg.author.tag, `requested a meme, but an error occured while trying to read memepool.json`);
+            } else if (!rawdata) {
+                msg.channel.send(`:x: I successfully read the file, but the data was not delivered to me as expected... whoopsie!`);
+                return Logger.log(msg.author.tag, `requested a meme, but the rawdata variable was empty.`);
             }
 
-            if (!rawdata) return msg.channel.send(':x: I successfully read the file, but the data was undefined... *again* :face_palm:');
-            
             const memepool = JSON.parse(rawdata);
-            
+
             let rand = Math.floor(Math.random() * memepool.urls.length);
             const url = memepool.urls[rand].location;
             const author = memepool.urls[rand].addedBy;
             const id = memepool.urls[rand].id;
 
-            let embed = new Discord.RichEmbed()
+            let embed = new Discord.MessageEmbed()
                 .setColor('#00FF15')
                 .setImage(url)
                 .setFooter(`Added by ${author} - ID: ${JSON.stringify(id)}`);
-
-            return msg.channel.send(embed);
+                
+            msg.channel.send(embed);
+            Logger.log(msg.author.tag, `meme succesfully delivered.`);
         });
-        
+
+
+                            /* OLD MEME.JS UPDATED TO USE v12 EMBEDS ON 01-09-2020 */
+        // fs.readFile('./commands/memepool.json', (err, rawdata) => {
+        //     if (err) {
+        //         msg.channel.send(`:x: An error occured trying to read the file, ${msg.author}.`);
+        //         throw new Error(err);
+        //     }
+
+        //     if (!rawdata) return msg.channel.send(':x: I successfully read the file, but the data was undefined... *again* :face_palm:');
+            
+        //     const memepool = JSON.parse(rawdata);
+            
+        //     let rand = Math.floor(Math.random() * memepool.urls.length);
+        //     const url = memepool.urls[rand].location;
+        //     const author = memepool.urls[rand].addedBy;
+        //     const id = memepool.urls[rand].id;
+
+        //     let embed = new Discord.RichEmbed()
+        //         .setColor('#00FF15')
+        //         .setImage(url)
+        //         .setFooter(`Added by ${author} - ID: ${JSON.stringify(id)}`);
+
+        //     return msg.channel.send(embed);
+        // });
     }
 }
