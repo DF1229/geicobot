44f02a184a0275763dd83bc7df1a3b87ed5f9b3b
DF1229 <daanfaber1@gmail.com>
2020-09-01 14:33:30 +0200

    Updated ban.js to use new logging system

--- commands/ban.js
+++ commands/ban.js
@@ -1,5 +1,5 @@
 const auditLogger = require('../custom_modules/auditLogEmbed.js');
-const Log = require('../custom_modules/consoleLog.js');
+const Logger = require('../custom_modules/logger.js');
 
 module.exports = {
     works: true,
@@ -13,7 +13,7 @@ module.exports = {
     adminOnly: false,
     execute(msg) {
         if (!msg.member.hasPermission('BAN_MEMBERS')) {
-            Log.misc(`${msg.author} failed banning ${msg.mentions.members.first()}`);
+            Logger.log(msg.author.tag), "used the ban command without the proper permissions.";
             return msg.channel.send(`:no_entry: You don't have the required permission(s) to use this command ${msg.author}`);
         }
 
@@ -22,13 +22,14 @@ module.exports = {
 
         msg.mentions.members.first().ban({ reason: `Issued by ${msg.author.tag}` }).then(() => {
             auditLogger.execute(msg, 'ban');
-            Log.action(msg, this.name);
+            Logger.log(msg.author.tag, `used ban command on ${taggedMember.user.tag}.`);
             return msg.channel.send(`:white_check_mark: User ${msg.mentions.members.first()} banned! :wave:`);
         }).catch(err => {
             if (err.name === 'DiscordAPIError' && err.message === 'Missing Permissions') {
                 msg.channel.send(`:x: I can't ban that user! This usually means they're an admin on this server ${msg.author} :face_palm:`);
+                return Logger.log(msg.author.tag, `tried to ban ${taggedMember.user.tag}, but their target was an admin.`);
             }
-            Log.misc(`${msg.author} failed banning ${msg.mentions.members.first()}`);
+            return Logger.log(msg.author.tag, `tried to ban ${taggedMember.user.tag}, but an unknown error occured.`);      
         });
     }
 };
