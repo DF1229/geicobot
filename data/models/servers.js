a5b289a3c698e8e3bbee5f2ffdf7f125524e65a9
DF1229 <daanfaber1@gmail.com>
2020-09-03 11:39:08 +0200

    Added database model

---
+++ data/models/servers.js
@@ -0,0 +1,34 @@
+const { DataTypes, Model } = require('sequelize');
+
+module.exports = class Servers extends Model {
+    static init(sequelize) {
+        return super.init({
+            guildID: {
+                type: DataTypes.DOUBLE,
+                unique: true
+            },
+            guildName: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            memberCount: {
+                type: DataTypes.INTEGER,
+                allowNull: false
+            },
+            prefix: {
+                type: DataTypes.STRING,
+                defaultValue: '>',
+                allowNull: false
+            },
+            economyEnabled: {
+                type: DataTypes.BOOLEAN,
+                defaultValue: false,
+                allowNull: false
+            }
+        }, {
+            tableName: 'Servers',
+            timestamps: true,
+            sequelize
+        });
+    }
+}
